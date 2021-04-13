1b701bf2b546dc0e5db2dd9d2859ed8705c83631
Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>
2021-04-13 20:23:25 +0530

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiaGl0YW5zaC1zaGFoIiwiYSI6ImNrbjMycnphMDFkYjcyb21mNDdheGhwZmcifQ._YbOWkiqdoJKZ49Dwfr_Jg&fuzzyMatch=false&limit=1';
+
+    if (address) {
+        request({ url: url, json: true }, (error, { body }) => {
+            if (error) {
+                callback('unable to connect to location services!', undefined)
+            }
+            else if (body.features.length === 0) {
+                callback('Unable to find location. Try another search.', undefined)
+            } else {
+                callback(undefined, {
+                    latitude: body.features[0].center[1],
+                    longitude: body.features[0].center[0],
+                    location: body.features[0].place_name
+                })
+            }
+        })
+    } else {
+        console.log('Please enter a location');
+    }
+
+
+}
+
+module.exports = geocode;
